72003905ded4219026313bf94b0e51f5809955a7
Gusttavo Soares <gustavosoares335@yahoo.com.br>
2025-09-30 15:06:57 -0300

    fix: add one more confirm payment

--- app.js
+++ app.js
@@ -52,16 +52,18 @@ app.post('/flow', async  (req, res) => {
 
   // Return the next screen & data to the client
   const screenData = {
-    screen: "CONFIRM_PAYMENT",
-    data: {
-      fornecedor: "Fornecedor Exemplo",
-      data_emissao: "29/09/2025",
-      data_vencimento: "29/10/2025",
-      valor_original: "1000",
-      descontos: "50",
-      descricao: "Pagamento referente a serviços",
-      tipo_documento: "Boleto",
-      numero_documento: "12345"
+      CONFIRM_PAYMENT: {
+      screen: "CONFIRM_PAYMENT",
+      data: {
+        fornecedor: "Fornecedor Exemplo",
+        data_emissao: "29/09/2025",
+        data_vencimento: "29/10/2025",
+        valor_original: "1000",
+        descontos: "50",
+        descricao: "Pagamento referente a serviços",
+        tipo_documento: "Boleto",
+        numero_documento: "12345"
+      }
     }
   };
 
